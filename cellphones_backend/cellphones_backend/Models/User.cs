973658a329cfba21b74974cd66d1a82c677c0108
VoTanThanhlaptrinh <votanthanh32004@gmail.com>
2025-07-09 17:08:49 +0700

    init

---
+++ cellphones_backend/cellphones_backend/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace cellphones_backend.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public DateOnly birthDay { get; set; }
+    }
+}
---
+++ cellphones_clone_ui/src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { AfterViewInit, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+
+export class HeaderComponent {
+
+}
